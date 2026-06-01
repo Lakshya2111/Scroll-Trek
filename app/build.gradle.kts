43a58e25f8e24494ef65b4aae39a6b0f834f9c5b
Lakshay Jain <lakshyaj21@gmail.com>
2026-06-01 23:18:44 +0530

    Fixed package name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.scrolltrek"
+        applicationId = "io.github.lakshya2111.scrolltrek"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
